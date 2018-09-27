554983dfaf065af130e5d9df633a6eafb9d0d9c7
Henry Withoot <osc2nuke@gmail.com>
2018-09-27 11:31:46 +0200

    Beta stage

--- admin/manufacturers.php
+++ admin/manufacturers.php
@@ -1,10 +1,9 @@
 <?php
 
-	require('includes/application_top.php');
+    require('includes/application_top.php');
 
-	require('includes/template_top.php');
+    require('includes/template_top.php');
     echo $oscTemplate->getContent('bread');
 
-	require('includes/template_bottom.php');
-	require('includes/application_bottom.php');
-?>
+    require('includes/template_bottom.php');
+    require('includes/application_bottom.php');
